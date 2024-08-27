396af6ef5c8f4bfd46ae25ddbd035a829e25ea4e
Abram Bradley <abramabradley@gmail.com>
2024-08-27 06:48:56 -0400

    Added plugin arguments

--- install.bash
+++ install.bash
@@ -2,7 +2,15 @@ set -euo pipefail
 
 OMZ_PLUGINS_DIR=~/.oh-my-zsh/custom/plugins
 
+PLUGINS=(
+    tamcore/autoupdate-oh-my-zsh-plugins
+    zsh-users/zsh-autosuggestions
+    zsh-users/zsh-syntax-highlighting
+    "$@"
+)
+
 OMZ_URL=https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
 AUTOUPDATE_REPO=https://github.com/tamcore/autoupdate-oh-my-zsh-plugins.git
 ZSH_AUTOSUGGEST_REPO=https://github.com/zsh-users/zsh-autosuggestions.git
 ZSH_SYNTAX_HIGHLIGHTING_REPO=https://github.com/zsh-users/zsh-syntax-highlighting.git
@@ -18,11 +26,26 @@ install_omz() {
     chsh -s /bin/zsh
 }
 
+install_omz2() {
+    curl -fsSL "$OMZ_URL" | sh
+
+    local plugin
+    for plugin in "${PLUGINS[@]}"; do
+        local package_name=${plugin##*/}
+        git clone "https://github.com/$plugin.git" "$OMZ_PLUGINS_DIR/$package_name"
+        sed -i \
+            "s/plugins=(\(.*\))/plugins=(\1 $package_name)/" \
+            ~/.zshrc
+    done
+
+    chsh -s /bin/zsh
+}
+
 if command -v apt-get >/dev/null; then
     apt-get update
     apt-get upgrade -y
     apt-get install -y zsh git curl
-    install_omz
+    install_omz2
 else
     echo 'Unrecognized package manager' >&2
 fi
